cef186036bc0550f31eaa2b9bea1d1f754d6250d
Athan <acheung4@mail.sfsu.edu>
2024-07-15 23:45:29 -0700

    index images

--- application/public/js/index.js
+++ application/public/js/index.js
@@ -1,15 +1,29 @@
+// function fadeOut(ev){
+//     let timer = setInterval(function(){
+//         console.log(ev.currentTarget);
+//         clearInterval(timer);
+//     }),50);
+// }
+
 function buildCardHTML(data) {
-    return;
+    return ` <div class="card">
+    <p class="card-title"${data.title}</p>
+    <img class="card-image"  src="${data.url}" alt="product image">
+    </div>`;
 }
 
 
 fetch("https://jsonplaceholder.typicode.com/albums/2/photos")
     .then(function (resp) {
-        resp.json();
+        return resp.json();
     })
     .then(function (resp_data) {
         let container = document.getElementById('container');
-        resp_data.forEach(function (photo)){
+        resp_data.forEach(function (photo) {
             container.innerHTML += buildCardHTML(photo);
-        }
+        });
+        document.getElementById('photo_count').textContent = resp_data.length;
     })
+    .catch(function (error) {
+        console.log(error);
+    });
