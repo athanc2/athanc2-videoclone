a99b02588ab75a7f7ca30292b8a6fef20ce5d7e1
Athan <acheung4@mail.sfsu.edu>
2024-07-17 01:58:32 -0700

    did m1

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+// localhost:3000/users
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name: "Athan Cheung" });
+});
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Login', name: "[insert name here]" });
 });
 
 module.exports = router;
