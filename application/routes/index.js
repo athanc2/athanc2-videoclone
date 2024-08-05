f96fce77dca7c2b4cdab657b69551b5aa2edf0ff
Athan <acheung4@mail.sfsu.edu>
2024-08-05 12:56:04 -0700

    index fix

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,13 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// localhost:3000/users
+// localhost:3000/
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name: "Athan Cheung" });
+  res.render('index.hbs', { title: 'CSC 317 Clone', name: "Athan Cheung" });
 });
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Login', name: "[insert name here]" });
+// login 
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+// register
+router.get('/register', function (req, res, next) {
+  res.render('register');
+});
+
+// postvideo
+router.get('/postvideo', function (req, res, next) {
+  res.render('postvideo');
+});
+
+// viewpost
+router.get('/viewpost', function (req, res, next) {
+  res.render('viewpost');
 });
 
 module.exports = router;
